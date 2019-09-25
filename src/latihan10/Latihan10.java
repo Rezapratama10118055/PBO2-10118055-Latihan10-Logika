b24d4afebd3dc5cf98f2f504b5d2aa0f4659a945
User <User@DESKTOP-2RC2UP5>
2019-09-25 08:53:07 +0700

    first commit

---
+++ src/latihan10/Latihan10.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan10;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur=19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("status dewasa:"+dewasa);
+        
+    }
+    
+}
